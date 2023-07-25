aca7b45e29ef54835c52545e848ae7d97f8bf9a9
delpfy <cubaru00@gmail.com>
2023-07-25 18:10:59 +0300

    fix

--- Validations/AddingItem.js
+++ Validations/AddingItem.js
@@ -6,6 +6,6 @@ export const addingItemValidator = [
     body('category', "Category should not be an number").isString(),
     body('price', "Price should not be an string, and it`s value shouldn`t be less than 0").isInt({min: 0}),
     body('sale', "Sale should not be an string, and it`s value should be in range from 0 to 100").isInt({min: 0, max: 100}),
-    body('rating', "Rating should not be an string").isNumeric(),
+    body('rating', "Rating should not be an string and it`s value should be in range from 0 to 5").isInt({min: 0, max: 5}),
     body('image', "More than one image required").isArray(),
 ]
--- index.js
+++ index.js
@@ -141,6 +141,7 @@ app.delete("/items/:id", checkAuthorization, checkRole, itemController.remove);
 app.patch(
   "/items/:id",
   checkAuthorization,
+  addingItemValidator,
   validationErrorsHandler,
   itemController.update
 );
