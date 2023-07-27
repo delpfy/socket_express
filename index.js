afe802234741eb763a0f9fb557437d65dba380ee
delpfy <cubaru00@gmail.com>
2023-07-27 16:09:45 +0300

    fix

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const app = express();
 
 app.use(function(req, res, next) {  
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE,OPTIONS');
   res.header(
     'Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, X-Api-Key'
   );
